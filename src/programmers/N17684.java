efcb52f69cb38c0ef0012d1257a82aa61f4f493a
hunko <rhwlgns4386@naver.com>
2023-08-14 17:21:30 +0900

    programmers N17684 압축

---
+++ src/programmers/N17684.java
@@ -0,0 +1,48 @@
+package programmers;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class N17684 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(Arrays.toString(solution.solution("KAKAO")));
+        System.out.println(Arrays.toString(solution.solution("TOBEORNOTTOBEORTOBEORNOT")));
+        System.out.println(Arrays.toString(solution.solution("ABABABABABABABAB")));
+    }
+
+    static class Solution {
+        public int[] solution(String msg) {
+            HashMap<String,Integer> map = new HashMap<>();
+            int count=1;
+            for(int i=0;i<26;i++){
+                char al= (char)('A'+i);
+                map.put(al+"",count++);
+            }
+            List<Character> collect = msg.chars().mapToObj((i) -> (char) i).collect(Collectors.toList());
+            LinkedList<Character> q = new LinkedList<>(collect);
+
+            ArrayList<Integer> result = new ArrayList<>();
+            while (!q.isEmpty()){
+                StringBuilder sb = new StringBuilder();
+                while (!q.isEmpty()){
+                    sb.append(q.peek());
+                    if(!map.containsKey(sb.toString())){
+                        map.put(sb.toString(),count++);
+                        sb.deleteCharAt(sb.length()-1);
+                        result.add(map.get(sb.toString()));
+                        break;
+                    }else{
+                        q.remove();
+                        if(q.isEmpty()){
+                            result.add(map.get(sb.toString()));
+                        }
+                    }
+                }
+            }
+
+            return result.stream().mapToInt(i->i).toArray();
+        }
+    }
+}
